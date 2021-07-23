0c07f0735401c66579fd4e081048c35d70963650
Sai Krishna <saikrishna@Administrators-MacBook-Pro.local>
2021-07-23 10:44:19 +0530

    Add new feature in Test features

--- src/main/java/com/itt/basetest/utils/TestFeatures.java
+++ src/main/java/com/itt/basetest/utils/TestFeatures.java
@@ -14,4 +14,5 @@ public final class TestFeatures {
 
 	public static final String OMS = "OMS";
 	public static final String ITRADEORDER = "ITRADEORDER";
+	public static final String OMSREWRITE = "OMSREWRITE";
 }
